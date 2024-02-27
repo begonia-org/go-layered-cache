40ae0e1434df09f0b4055ce7d326818d8f7342bb
qinzhonghe <qinzhonghe@51qqt.com>
2024-02-27 17:32:49 +0800

    feat:add bloom

---
+++ bloom.go
@@ -0,0 +1,110 @@
+package golayeredbloom
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"sync"
+
+	"github.com/bits-and-blooms/bloom/v3"
+)
+
+type bloomFilter struct {
+	*bloom.BloomFilter
+}
+
+// baseHashes returns the four hash values of data that are used to create k
+// hashes
+func BaseHashes(data []byte) [4]uint64 {
+	var d digest128 // murmur hashing
+	hash1, hash2, hash3, hash4 := d.sum256(data)
+	return [4]uint64{
+		hash1, hash2, hash3, hash4,
+	}
+}
+
+// location returns the ith hashed location using the four base hash values
+func location(h [4]uint64, i uint) uint64 {
+	ii := uint64(i)
+	return h[ii%2] + ii*h[2+(((ii+(ii%2))%4)/2)]
+}
+
+func newBloomFilter(n uint, p float64) *bloomFilter {
+	return &bloomFilter{bloom.NewWithEstimates(n, p)}
+}
+func (bf *bloomFilter) location(h [4]uint64, i uint) uint {
+	return uint(location(h, i) % uint64(bf.Cap()))
+}
+func (bf *bloomFilter) Locations(data []byte) []uint {
+	h := BaseHashes(data)
+	locations := make([]uint, bf.K())
+	for i := uint(0); i < uint(bf.K()); i++ {
+		loc := bf.location(h, i)
+		locations[i] = loc
+	}
+	return locations
+}
+
+type LayeredBloomFilter struct {
+	pubsub     BloomPubSub
+	filters    map[string]*bloomFilter
+	mux        sync.Mutex
+	name       string
+	pubsubName string
+}
+
+func NewLayeredBloomFilter(pubsub BloomPubSub, pubsubName, name string) *LayeredBloomFilter {
+	return &LayeredBloomFilter{
+		pubsub:     pubsub,
+		filters:    make(map[string]*bloomFilter),
+		name:       name,
+		pubsubName: pubsubName,
+	}
+}
+
+func (bf *LayeredBloomFilter) TestOrAdd(ctx context.Context, key string, value []byte, n uint, p float64) (bool, error) {
+	bf.mux.Lock()
+	defer bf.mux.Unlock()
+	filter, ok := bf.filters[key]
+	if !ok {
+		filter = newBloomFilter(n, p)
+		bf.filters[key] = filter
+	}
+	ok = filter.TestOrAdd(value)
+	if !ok {
+		locations := filter.Locations(value)
+		err := bf.cache(ctx, key, locations)
+		log.Println("cache err:", err)
+		return ok, err
+	}
+	return ok, nil
+
+}
+func (bf *LayeredBloomFilter) appendBitSet(ctx context.Context, key string, values []uint) error {
+	bf.mux.Lock()
+	defer bf.mux.Unlock()
+	filter, ok := bf.filters[key]
+	if !ok {
+		return fmt.Errorf("No filter found for key: %s", key)
+	}
+	for _, val := range values {
+		filter.BitSet().Set(val)
+	}
+	return nil
+}
+func (bf *LayeredBloomFilter) cache(ctx context.Context, key string, values []uint) error {
+	message := &BloomBroadcastMessage{
+		From:      bf.name,
+		BloomKey:  key,
+		Locations: values,
+	}
+	return bf.pubsub.Publish(ctx, bf.pubsubName, message)
+}
+
+func (bf *LayeredBloomFilter) watch(ctx context.Context) <-chan error {
+	return bf.pubsub.Subscribe(ctx, bf.pubsubName, bf)
+}
+func (bf *LayeredBloomFilter) OnStart(ctx context.Context) <-chan error {
+	errChan := bf.watch(ctx)
+	return errChan
+}
---
+++ pubsub.go
@@ -0,0 +1,120 @@
+package golayeredbloom
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"runtime"
+
+	"github.com/redis/go-redis/v9"
+	"github.com/sirupsen/logrus"
+)
+
+type BloomPubSub interface {
+	Publish(ctx context.Context, channel string, message *BloomBroadcastMessage) error
+	Subscribe(ctx context.Context, channel string, bg *LayeredBloomFilter) <-chan error
+}
+
+type bloomPubSubImpl struct {
+	rdb          *redis.Client
+	consumerName string
+	groupName    string
+	log          *logrus.Logger
+}
+type BloomBroadcastMessage struct {
+	// The message to be broadcast.
+	From      string `json:"from"`
+	BloomKey  string `json:"bloom_key"`
+	Locations []uint `json:"locations"`
+}
+
+func NewBloomPubSub(rdb *redis.Client, group, consumer string, log *logrus.Logger) BloomPubSub {
+	return &bloomPubSubImpl{rdb: rdb, consumerName: consumer, groupName: group, log: log}
+
+}
+
+// Publish broadcasts a message to a channel.
+func (b *bloomPubSubImpl) Publish(ctx context.Context, channel string, message *BloomBroadcastMessage) error {
+	pipeline := b.rdb.TxPipeline()
+	for _, location := range message.Locations {
+		pipeline.SetBit(ctx, message.BloomKey, int64(location), 1)
+	}
+	pipeline.XAdd(ctx, &redis.XAddArgs{
+		Stream: channel,
+		Values: message,
+	})
+	_, err := pipeline.Exec(ctx)
+	return err
+}
+func (b *bloomPubSubImpl) appendBitSet(ctx context.Context, message map[string]interface{}, bf *LayeredBloomFilter) error {
+	// 将map转换为JSON字节序列
+	jsonData, err := json.Marshal(message)
+	if err != nil {
+		return fmt.Errorf("Error marshalling map to json: %w", err)
+	}
+
+	// 将JSON解码到结构体中
+	var p BloomBroadcastMessage
+	err = json.Unmarshal(jsonData, &p)
+	if err != nil {
+
+		return fmt.Errorf("Error unmarshalling json to struct:%w", err)
+	}
+	return bf.appendBitSet(ctx, p.BloomKey, p.Locations)
+
+}
+
+func (b *bloomPubSubImpl) Subscribe(ctx context.Context, channel string, bf *LayeredBloomFilter) <-chan error {
+	errChan := make(chan error)
+	go func() {
+		defer close(errChan)
+
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				// XReadGroup命令参数
+				args := redis.XReadGroupArgs{
+					Group:    b.groupName,
+					Consumer: b.consumerName,
+					Streams:  []string{channel, "0-0"},
+					Block:    0, // 0表示无限期阻塞直到消息到来
+					Count:    1, // 每次读取一个消息
+
+				}
+
+				// 阻塞等待消息
+				messages, err := b.rdb.XReadGroup(ctx, &args).Result()
+				if err != nil {
+					b.log.Errorf("Error reading from stream: %v", err)
+					errChan <- err
+					continue
+				}
+
+				// 处理收到的消息
+				for _, message := range messages {
+					for _, msg := range message.Messages {
+						// 确认消息处理
+						_, err := b.rdb.XAck(ctx, channel, b.groupName, msg.ID).Result()
+						if err != nil {
+							errChan <- err
+							b.log.Errorf("Error acknowledging message: %v", err)
+						}
+						// 处理消息
+						err = b.appendBitSet(ctx, msg.Values, bf)
+						if err != nil {
+							errChan <- err
+							b.log.Errorf("Error processing message: %v", err)
+						}
+					}
+				}
+
+			}
+			// 释放CPU
+			runtime.Gosched()
+
+		}
+	}()
+	return errChan
+}
