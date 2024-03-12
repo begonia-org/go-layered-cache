e888e2200e7fd2f89ff163a0d4973f629fbf3385
vforfreedom96@gmail.com <vforfreedom96@gmail.com>
2024-03-12 10:23:17 +0800

    chore:add doc.go

---
+++ doc.go
@@ -0,0 +1,6 @@
+/*
+Package golayeredcache implements a Golang multi-level caching framework based on Redis and local memory for distributed scenarios.
+
+See https://github.com/begonia-org/go-layered-cache for more information about go-layered-cache.
+*/
+package golayeredcache // import "github.com/begonia-org/go-layered-cache"
